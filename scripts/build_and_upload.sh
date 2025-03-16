541bdf75a8c211dbb98748f8636fe8aa225ad471
Christopher Tavolazzi <ctavolazzi@gmail.com>
2025-03-16 09:17:07 -0700

    Update build script to use python3 commands

--- scripts/build_and_upload.sh
+++ scripts/build_and_upload.sh
@@ -10,16 +10,16 @@ echo "Building code-conductor v${VERSION} package..."
 rm -rf dist/ build/ *.egg-info/
 
 # Build the package
-python -m pip install --upgrade build
-python -m build
+python3 -m pip install --upgrade build
+python3 -m build
 
 echo "Build complete. Package files:"
 ls -la dist/
 
 echo "Ready to upload to PyPI"
-echo "To upload, run: python -m twine upload dist/*"
+echo "To upload, run: python3 -m twine upload dist/*"
 echo ""
-echo "Make sure you have twine installed: pip install twine"
-echo "If you need a test upload first, run: python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
+echo "Make sure you have twine installed: pip3 install twine"
+echo "If you need a test upload first, run: python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
 echo ""
-echo "After uploading, you can verify the installation with: pip install --upgrade code-conductor"
+echo "After uploading, you can verify the installation with: pip3 install --upgrade code-conductor"
